c4f39732e6e60c9bcaad5f14b20ef2668620c1c0
DERYKO <derykowaynx@gmail.com>
2019-05-26 12:29:11 +0300

    few api changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,8 +21,7 @@ class LoginController extends Controller
         if (!$validator->fails()){
             if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
                 $user = Auth::user();
-                $success['token'] =  $user->createToken('MyApp')-> accessToken;
-                return response()->json(['success' => $success], $this-> successStatus);
+                return response()->json(['success' => $user]);
             }
             else{
                 return response()->json(['error'=>'Unauthorised'], 401);
