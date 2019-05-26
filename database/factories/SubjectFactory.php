5aa5290c5fac4e832c6f546c8a80248414f70787
DERYKO <derykowaynx@gmail.com>
2019-05-26 15:30:19 +0300

    few api changes

--- database/factories/SubjectFactory.php
+++ database/factories/SubjectFactory.php
@@ -9,6 +9,6 @@ $factory->define(\App\Subject::class, function (Faker $faker,$topic_id) {
     return [
         'topic_id'=>$topic_id,
         'subject_name' => $faker->sentence,
-        'subject_avatar_url' => $faker->imageUrl(150,150,true,null,true)
+        'subject_avatar_url' => $faker->imageUrl(150, 150, 'cats', true, true, 'Faker')
     ];
 });
