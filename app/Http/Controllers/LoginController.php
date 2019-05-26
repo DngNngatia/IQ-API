c759ab78047412bef58f8acfde0522320a4847ad
DERYKO <derykowaynx@gmail.com>
2019-05-26 12:21:46 +0300

    few api changes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
@@ -17,24 +19,22 @@ class LoginController extends Controller
             'password' => 'required|string'
         ]);
         if (!$validator->fails()){
-            $credentials = request(['email', 'password']);
-            if (!Auth::attempt($credentials))
-                return response()->json([
-                    'message' => 'Unauthorized'
-                ], 401);
-            $user = $request->user();
-            $tokenResult = $user->createToken('Personal Access Token');
-            $token = $tokenResult->token;
-            if ($request->remember_me)
-                $token->expires_at = Carbon::now()->addWeeks(1);
-            $token->save();
-            return response()->json([
-                'access_token' => $tokenResult->accessToken,
-                'token_type' => 'Bearer',
-                'expires_at' => Carbon::parse(
-                    $tokenResult->token->expires_at
-                )->toDateTimeString()
-            ]);
+            $user = User::where('email', $request->get('email'))->first();
+            if($user){
+                if(Hash::check( $request->get('password'), $user->password )== false){
+                    return response()->json(['message' => 'invalid password'], 401);
+                }else{
+                    $tokenResult = $user->createToken('Personal Access Token');
+                    $token = $tokenResult->token;
+                    $token->save();
+                    return response()->json([
+                        'access_token' => $tokenResult->accessToken,
+                        'token_type' => 'Bearer',
+                    ]);
+                }
+            }else{
+                return response()->json(['message' => 'User does not exists!!!'], 401);
+            }
         }else{
             return response()->json([
                 'message' => 'field error',
