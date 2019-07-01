8f74caf82a84402c89ab3737b7030b147340d02c
derrick <derykowaynx@gmail.com>
2019-07-01 15:26:19 +0300

    delete field attempts from subjects

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -3,33 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Score;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ScoreController extends Controller
 {
 
     public function store(Request $request, $subject_id)
     {
-        if (Score::where('subject_id',$subject_id)->where('user_id',$request->user()->id)->exists()){
-            $score=Score::where('subject_id',$subject_id)->where('user_id',$request->user()->id)->first();
-            $score->update([
-               'score' => $request->input("score")
-            ]);
-            $score->subject->update([
-                'attempts'  => $score->attempts++
-            ]);
-        }else{
-            $score = Score::create([
-                'user_id' => $request->user()->id,
-                'subject_id' => $subject_id,
-                'complete' => true,
-                'score' => $request->input("score")
-            ]);
-            $score->subject->update([
-                'attempts'  => $score->attempts++
-            ]);
-            return response()->json(["message" => 'success', "data" => $score]);
-        }
+        $score = Score::create([
+            'user_id' => $request->user()->id,
+            'subject_id' => $subject_id,
+            'complete' => true,
+            'score' => $request->input("score")
+        ]);
 
+        return response()->json(["message" => 'success', "data" => $score]);
+
+    }
+
+    public function subjects()
+    {
+        $scores = collect(User::findOrFail(Auth::id())->score)->each(function ($score) {
+            return $score->subject;
+        })->toArray();
+        return response()->json(["message" => 'success', "data" => $scores]);
     }
 }
