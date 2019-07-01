fecb458d75b387bc746d26cbf27b76b4f9ff638a
derrick <derykowaynx@gmail.com>
2019-07-01 16:09:12 +0300

    okay attempts

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -12,17 +12,25 @@ class ScoreController extends Controller
 
     public function store(Request $request, $subject_id)
     {
-        $score = Score::create([
-            'user_id' => $request->user()->id,
-            'subject_id' => $subject_id,
-            'complete' => true,
-            'score' => $request->input("score")
-        ]);
-
-        return response()->json(["message" => 'success', "data" => $score]);
-
+        if (Score::where('subject_id', $subject_id)->where('user_id', $request->user()->id)->exists()) {
+            $score = Score::where('subject_id', $subject_id)->where('user_id', $request->user()->id)->first();
+            $score->update([
+                'score' => $request->input("score"),
+                'attempts' => $score->attempts++
+            ]);
+        } else {
+            $score = Score::create([
+                'user_id' => $request->user()->id,
+                'subject_id' => $subject_id,
+                'complete' => true,
+                'score' => $request->input("score")
+            ]);
+            $score->update([
+                'attempts' => $score->attempts++
+            ]);
+            return response()->json(["message" => 'success', "data" => $score]);
+        }
     }
-
     public function subjects()
     {
         $scores = collect(User::findOrFail(Auth::id())->score)->each(function ($score) {
