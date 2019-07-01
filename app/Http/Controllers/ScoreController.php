d4659a853f466f22480bb0d27b4a6f99dfcc85f4
derrick <derykowaynx@gmail.com>
2019-07-01 15:20:09 +0300

    attempts logic

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -10,16 +10,26 @@ class ScoreController extends Controller
 
     public function store(Request $request, $subject_id)
     {
-        $score = Score::createOrUpdate([
-            'user_id' => $request->user()->id,
-            'subject_id' => $subject_id,
-            'complete' => true,
-            'score' => $request->input("score")
-        ]);
-        $score->subject->update([
-          'attempts'  => $score->attempts++
-        ]);
-        return response()->json(["message" => 'success', "data" => $score]);
+        if (Score::where('subject_id',$subject_id)->where('user_id',$request->user()->id)->exists()){
+            $score=Score::where('subject_id',$subject_id)->where('user_id',$request->user()->id)->first();
+            $score->update([
+               'score' => $request->input("score")
+            ]);
+            $score->subject->update([
+                'attempts'  => $score->attempts++
+            ]);
+        }else{
+            $score = Score::create([
+                'user_id' => $request->user()->id,
+                'subject_id' => $subject_id,
+                'complete' => true,
+                'score' => $request->input("score")
+            ]);
+            $score->subject->update([
+                'attempts'  => $score->attempts++
+            ]);
+            return response()->json(["message" => 'success', "data" => $score]);
+        }
 
     }
 }
