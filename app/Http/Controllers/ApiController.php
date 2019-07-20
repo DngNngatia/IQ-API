a693eec607f67ba4cb803d1fdb48c712b5adac19
derrick <derykowaynx@gmail.com>
2019-07-20 22:29:25 +0300

    like and dislike

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -34,7 +34,7 @@ class ApiController extends Controller
                 'likes' => count($subject->likes),
                 'dislikes' => count($subject->dislikes),
                 'no_comments' => count($subject->comments),
-                'comments' => $subject->comments->with('user'),
+                'comments' => $subject->comments->user,
                 'score' => Score::where('user_id', $request->user()->id)->where('subject_id', $subject->id)->first()
             ];
         })->forPage(1, 3);
