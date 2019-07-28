5735be0284aa92905bc4765ae24eb68ce1bf4489
derrick <derykowaynx@gmail.com>
2019-07-28 14:09:50 +0300

    reset password mailing

--- resources/views/mails/reset.blade.php
+++ resources/views/mails/reset.blade.php
@@ -1,4 +1,5 @@
 <h1>Hi, {{ $data['name']}}</h1>
+<img src="https://raw.githubusercontent.com/DERYKO/noprex/master/src/images/logo.png" height="90px">
 <p>Request to reset your password has been received</p>
 <p>Otp: {{$data['otp']}}</p>
 <p>If you did no request to reset your password please ignore this message, Regards Noprex Team</p>
