2d9d39626c66f67d0856ff3b51841fcf4514fcf7
derrick <derykowaynx@gmail.com>
2019-07-30 11:58:16 +0300

    mail

--- app/Mail/SendMailable.php
+++ app/Mail/SendMailable.php
@@ -30,6 +30,7 @@ class SendMailable extends Mailable implements ShouldQueue
      */
     public function build()
     {
-        return $this->view('mail');
+        return $this->from('example@example.com')
+        ->view('mail');
     }
 }
